102b862ddb93ed8d3ab5b92511f8f108dd5b0206
Shahrizan <shahrizan.learner@gmail.com>
2022-11-27 09:13:57 +0700

    feat: add comment to DatabaseAkun class

--- src/DatabaseAkun.java
+++ src/DatabaseAkun.java
@@ -8,12 +8,14 @@ public class DatabaseAkun {
     private ArrayList<Akun> databaseAkun = new ArrayList<>(); // array bertipe Akun
 
     public DatabaseAkun() {
-        //membuat akun nasabah dan admin
+        // membuat akun nasabah dan admin
         Akun AkunNasabah1 = new AkunNasabah("Shahrizan", "12345", "11111", 10_000_000);
         Akun AkunNasabah2 = new AkunNasabah("Alif Putra Cira", "98765", "22222", 20_000_000);
         Akun AkunNasabah3 = new AkunNasabah("Raffi Firdaus", "19234", "33333", 30_000_000);
         Akun AkunAdmin4 = new AkunAdmin("Nanda Aulia Shofiah", "00000", "12022", true);
         Akun AkunAdmin5 = new AkunAdmin("Ishlah Putri Ariyani", "99999", "20045", true);
+        // menambahkan objek AkunNasabah dan AkunAdmin ke array databaseAkun menggunakan method khusus arraylist
+        // yaitu add
         databaseAkun.add(AkunNasabah1);
         databaseAkun.add(AkunNasabah2);
         databaseAkun.add(AkunNasabah3);
@@ -22,11 +24,19 @@ public class DatabaseAkun {
     }
 
     public Akun getAkun(String nomorRekening, String pin) {
+        // melakukan perulangan for each yang mana setiap data pada attribute databaseAkun
+        // akan dimasukkan ke variabel akun bertipe Akun
+        // dan akan melakukan perulangan dari indeks 0 sampai paling akhir
         for (Akun akun : databaseAkun) {
+            // melakukan pengecekan pada setiap objek Akun yang tersimpan di arraylist databaseAkun
+            // jika nilai argument nomorRekening dan pin cocok dengan nilai yang ada di attribute akun
+            // maka kembalikan objek akun tersebut
             if (akun.validasiNomorRekening(nomorRekening) && akun.validasiPin(pin)) {
                 return akun;
             }
         }
+        // jika nilai argument nya tidak cocok dengan nilai yang ada di attribute objek akun
+        // maka kembalikan null
         return null;
     }
 }
