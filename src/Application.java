b994225f5bd9dd6133ae4a8db310eee8725f678b
Shahrizan <shahrizan.learner@gmail.com>
2022-11-27 09:44:59 +0700

    fix: remove extend atm in class application

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,9 @@
-public class Application extends ATM {
+public class Application {
     public static void main(String[] args) {
-        ATM atm = new ATM();
         System.out.println("|====================================|");
         System.out.println("|~~~Selamat datang di ATM Bank WKG~~~|");
         System.out.println("|====================================|");
+        ATM atm = new ATM();
         atm.login();
     }
 }
